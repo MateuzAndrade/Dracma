29466235e671f38db5268c9d84d0cdc9691c8f8e
Mateus Andrade <mateus.nascimento.andrade@gmail.com>
2025-02-10 06:52:00 -0300

    Project Start point zero

---
+++ dracma backend/src/main/java/com/pegmatita/dracma/DracmaApplication.java
@@ -0,0 +1,13 @@
+package com.pegmatita.dracma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DracmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DracmaApplication.class, args);
+	}
+
+}
